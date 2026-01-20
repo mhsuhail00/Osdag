886f270734d653900ed46941cb34120bd63535b1
nishikantmandal007 <nishikantmandal007@gmail.com>
2026-01-20 22:29:03 +0530

    refactor: Centralize memory cleanup using CleanupCoordinator and optimize CAD model generation by returning lists of shapes instead of fused objects.

--- src/osdag_core/cad/common_logic.py
+++ src/osdag_core/cad/common_logic.py
@@ -3103,13 +3103,6 @@ class CommonDesignLogic(object):
                     osdag_display_shape(self.display, welds, update=True, color=weld_color, label=label_weld, canvas=self.cad_widget)
 
     def call_3DModel(self, flag, module_object):  # Done
-        # CRITICAL memory cleanup:
-        try:
-            from osdag_gui.OS_safety_protocols import get_occ_memory_manager
-            get_occ_memory_manager().safe_cleanup(id(self.cad_widget))
-        except Exception:
-            pass
-
         self.module_object = module_object  # Store the object directly
         print(self.mainmodule)
 
